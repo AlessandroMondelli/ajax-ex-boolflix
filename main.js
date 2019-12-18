1f060a81d7f4d1b741310255278f6a2341a14975
Alessandro_Mondelli <alexs98@hotmail.it>
2019-12-18 10:58:43 +0100

    Ottimizzate funzioni per voti/stelle e ricerca serie/film

--- main.js
+++ main.js
@@ -71,41 +71,38 @@ $(document).ready(function() {
         }
     }
 
-    function appendFilm(filmArr,flag) { //Funzione che stampa film trovati
-        var filmPosition = $(".results.film"); //salvo la posizione dove dovranno essere inseriti i film
-        var seriePosition = $(".results.series"); //salvo la posizione dove dovranno essere inserite le serie
-
+    function appendFilm(filmArr,flagF) { //Funzione che stampa film trovati
         for (var i = 0; i < filmArr.length; i++) { //Scorro tutti i film trovati con la ricerca
             var starsVote = voteToStars(filmArr[i].vote_average); //Richiamo funzione per trasformare voto in stelle
             // var flag = setFlags(filmArr[i].original_language);
-            if (flag == true) { //Se è un film...
-                var printTemplate = objTemplate(filmArr[i],starsVote); //Chiamo funzione per scrivere template film
-                printHtml(printTemplate,filmPosition); //richiamo funzione per appendere film nell'html
-            } else { //Se è una serie...
-                var printTemplate = objTemplateSerie(filmArr[i],starsVote); //Chiamo funzione per scrivere template serie
-                printHtml(printTemplate,seriePosition); //richiamo funzione per appendere serie nell'html
+            var printAndPos = objTemplate(filmArr[i],starsVote,flagF); //Chiamo funzione per scrivere template film
+            console.log(printAndPos);
+            printHtml(printAndPos[0],printAndPos[1]); //richiamo funzione per appendere film nell'html
+        }
+    }
+
+    function objTemplate(actFilm,stampVote,flagFS) { //Funzione che prepara template film
+        var infoAndPosition = [];
+        if (flagFS == true) {
+            var print = { //Oggetto per prendere variabili Handlebars
+                boolTitle : actFilm.title, //recupero titolo
+                boolOrTitle : actFilm.original_title, //Recupero titolo originale
+                // boolLang : flag, //Bandiera lingua
+                boolVote : stampVote //Voto in stelle
             }
+            var filmPosition = $(".results.film"); //salvo la posizione dove dovranno essere inseriti i film
+            infoAndPosition.push(print,filmPosition);
+        } else {
+            var print = { //Oggetto per prendere variabili Handlebars
+                boolTitle : actFilm.name, //recupero titolo
+                boolOrTitle : actFilm.original_name, //Recupero titolo originale
+                // boolLang : flag, //Bandiera lingua
+                boolVote : stampVote //Voto in stelle
+            }
+            var seriePosition = $(".results.series"); //salvo la posizione dove dovranno essere inserite le serie
+            infoAndPosition.push(print,seriePosition);
         }
-    }
-
-    function objTemplate(actFilm,stampVote) { //Funzione che prepara template film
-        var print = { //Oggetto per prendere variabili Handlebars
-            boolTitle : actFilm.title, //recupero titolo
-            boolOrTitle : actFilm.original_title, //Recupero titolo originale
-            // boolLang : flag, //Bandiera lingua
-            boolVote : stampVote //Voto in stelle
-        }
-        return print;
-    }
-
-    function objTemplateSerie(actFilm,stampVote) { //Funzione che prepara template serie
-        var print = { //Oggetto per prendere variabili Handlebars
-            boolTitle : actFilm.name, //recupero titolo
-            boolOrTitle : actFilm.original_name, //Recupero titolo originale
-            // boolLang : flag, //Bandiera lingua
-            boolVote : stampVote //Voto in stelle
-        }
-        return print;
+        return infoAndPosition
     }
 
     function printHtml(templateP,position) { //Funzione he stampa su html
@@ -114,63 +111,32 @@ $(document).ready(function() {
     }
 
     function voteToStars(vote) { //Funzione che restituisce il voto in stelle
+        var vote5; //Voto in base 5
         var retVote; //Variabile che conterrà il contenuto da mandare a schermo
 
-        if (vote == 0) { //Se il voto è zero...
-            retVote = forStars(0,5); //Richiamo funzione per concatenare le varie stelle (5 vuote)
-        } else if (vote >= 1 && vote < 3) { //Se il voto è compreso tra 1 e 3...
-            retVote = forStars(1,4);  //Richiamo funzione per concatenare le varie stelle (1 piena e 4 vuote)
-        } else if (vote >= 3 && vote < 5) { //Se il voto è compreso tra 3 e 5...
-            retVote = forStars(2,3);  //Richiamo funzione per concatenare le varie stelle (2 piene e 3 vuote)
-        } else if (vote >= 5 && vote < 7) { //Se il voto è compreso tra 5 e 7...
-            retVote = forStars(3,2);  //Richiamo funzione per concatenare le varie stelle (3 piene e 2 vuote)
-        } else if (vote >= 7 && vote < 9) { //Se il voto è compreso tra 7 e 9...
-            retVote = forStars(4,1);  //Richiamo funzione per concatenare le varie stelle (4 piene e 1 vuota)
-        } else if (vote >= 9 && vote < 10) { //Se il voto è compreso tra 9 e 10...
-            retVote = forStars(5,0);  //Richiamo funzione per concatenare le varie stelle (5 piene)
-        }
+        vote5 = Math.ceil((vote / 2)); //Arrontondo voto in base 5
+        retVote = forStars(vote5); //Richiamo funzona per trasformare voto in stelle
 
         return retVote; //Ritorno codice con stelle
     }
 });
 
-function forStars(full,empty) { //Funzione che permette di immettere in una variabile un tot di stelle piene e vuote
+function forStars(full) { //Funzione che permette di immettere in una variabile un tot di stelle piene e vuote
     var fullStar = "<i class=\"fas fa-star\"></i>"; //Codice per stella piena
     var emptyStar = "<i class=\"far fa-star\"></i>" //Codice per stella vuota
     var finalVote = ""; //Codice che ocnterrà tutte le stelle
 
-    for (var i = 0; i < full; i++) { //For per scrivere stelle piene
-        finalVote = finalVote.concat(fullStar); //Concateno tutto nella variabile del voto finale
+    for (var i = 0; i < 5; i++) { //For per scrivere stelle piene
+        if (i < full) {
+            finalVote = finalVote.concat(fullStar);//Concateno stelle piene nella variabile del voto finale
+        } else {
+            finalVote = finalVote.concat(emptyStar); //Concateno stelle piene nella variabile del voto finale
+        }
     }
-
-    for (var i = 0; i < empty; i++) { //For per scrivere stelle vuote
-        finalVote = finalVote.concat(emptyStar); //Concateno tutto nella variabile del voto finale
-    }
-
     return finalVote; //Ritorno codice finale
 }
 
     //Non Funzionante
 // function setFlags(flagReq) {
-//     var urlFlags = 'https://public-us.opendatasoft.com/api/records/1.0/search/?dataset=country-flags';
-//
-//     $.ajax ({ //Chiamata AJAX per recuperare bandiere
-//         'url' : urlFlags,
-//         'method' : 'GET', //Metodo GET
-//         'success' : function(flagRes) {  //Caso funzionamento richiesta
-//             flags = flagRes.response;
-//             fetch(urlFlags)
-//                 .then(flags => flags.json())
-//                 .then(records => {
-//                     records.forEach(function(record) {
-//                         $(record.fields)
-//                     })
-//
-//
-//                 });
-//         },
-//         'error' : function() { //Caso di errore di caricamento
-//             alert("Errore");
-//         }
-//     });
+
 // }
